3aae816dbaadfe1841ec7df9aea7568c03fda456
KO <ko25july@gmail.com>
2025-08-03 09:22:57 +0700

    Update 60019470-55e7.js

--- 60019470-55e7.js
+++ 60019470-55e7.js
@@ -1,1 +1,9 @@
-(function () { console.log("555"); return "555"; })();
+(function () {
+  const wifi = require("Wifi");
+  let oldHostName = wifi.getHostname();
+  wifi.setHostname("switch")
+  let newHostName = wifi.getHostname();
+  console.log("Old Host Name:", `${oldHostName}.local`);
+  console.log("New Host Name:", `${newHostName}.local`);
+  return newHostName;
+})();
