9affdfdadd718b60c1e80a67c9889e507eafd4e7
KO <ko25july@gmail.com>
2025-08-11 20:37:27 +0700

    Update 60019470-55e7.js

--- 60019470-55e7.js
+++ 60019470-55e7.js
@@ -1,1 +1,7 @@
-(function () {})();
+(function () {
+  require("code.js").httpRequest({
+    url: require("code.js").readJSON().TELEGRAME_URL,
+    data: require("code.js").readJSON().TELEGRAME_DATA,
+    headers: { "Content-Type": "application/x-www-form-urlencoded" }
+  });
+})();
