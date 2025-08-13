3bf952a85622d97115a42970f2aa6265d7a467c0
KO <ko25july@gmail.com>
2025-08-13 09:15:45 +0700

    Update 60019470-55e7.js

--- 60019470-55e7.js
+++ 60019470-55e7.js
@@ -1,7 +1,5 @@
-(function () {
-  httpRequest({
-    url: storage.read("telegrame.txt") + "/sendMessage",
-    data: "chat_id=7928495281&text=สวิตซ์ประตูไฟฟ้าเชื่อมต่อแล้ว (60019470-55E7)",
-    headers: { "Content-Type": "application/x-www-form-urlencoded" }
-  });
-})();
+httpRequest({
+  url: require("Storage").read("telegrame.txt") + "/sendMessage",
+  data: "chat_id=7928495281&text=สวิตซ์ประตูไฟฟ้าเชื่อมต่อแล้ว (60019470-55E7)",
+  headers: { "Content-Type": "application/x-www-form-urlencoded" }
+});
