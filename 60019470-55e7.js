55cea0ca5130241fd3ba83b9fbc06b87af5c65ba
KO <ko25july@gmail.com>
2025-11-23 11:01:00 +0700

    Update message texts for web socket events

--- 60019470-55e7.js
+++ 60019470-55e7.js
@@ -1,6 +1,24 @@
 httpRequest({
   method: "POST",
   url: require("Storage").read("telegrame.txt") + "/sendMessage",
-  data: "chat_id=7928495281&text=สวิตซ์ประตูไฟฟ้าเชื่อมต่อแล้ว (60019470-55E7)",
+  data: "chat_id=7928495281&text=สวิตซ์ประตูไฟฟ้าพร้อมทำงานแล้ว",
   headers: { "Content-Type": "application/x-www-form-urlencoded" }
+});
+
+ws.on("open", function () {
+  setTimeout(httpRequest, 1000, {
+    method: "POST",
+    url: require("Storage").read("telegrame.txt") + "/sendMessage",
+    data: "chat_id=7928495281&text=สวิตซ์ประตูไฟฟ้าไดัเริ่มการเชื่อมต่อแล้ว",
+    headers: { "Content-Type": "application/x-www-form-urlencoded" }
+  });
+});
+
+ws.on("close", function () {
+  setTimeout(httpRequest, 1000, {
+    method: "POST",
+    url: require("Storage").read("telegrame.txt") + "/sendMessage",
+    data: "chat_id=7928495281&text=สวิตซ์ประตูไฟฟ้าได้หยุดการเชื่อมต่อแล้ว",
+    headers: { "Content-Type": "application/x-www-form-urlencoded" }
+  });
 });
