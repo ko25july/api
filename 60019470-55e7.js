0ec446235618639af9dae8442b0b9d59f3bf0e97
KO <ko25july@gmail.com>
2025-11-25 09:19:40 +0700

    Refactor data handling in LoopbackA event

--- 60019470-55e7.js
+++ 60019470-55e7.js
@@ -5,17 +5,34 @@ httpRequest({
   headers: { "Content-Type": "application/x-www-form-urlencoded" }
 });
 
-Serial1.on("data", function (data) {
-  let index = data.indexOf("httpRequest");
+LoopbackA.on("data", function (data) {
+  if (data.includes("httpRequest")) return;
 
-  if (index >= 0) return;
+  data = data.slice(2, -2).replaceAll("[J", "");
 
-  data = data.slice(index, -2).replaceAll("[J", "");
-
-  setTimeout(httpRequest, 3000, {
+  let idTimeout = setTimeout(httpRequest, 3000, {
     method: "POST",
     url: require("Storage").read("telegrame.txt") + "/sendMessage",
     data: "chat_id=7928495281&text=สวิตซ์ประตูไฟฟ้า: " + data,
     headers: { "Content-Type": "application/x-www-form-urlencoded" }
   });
+
+  if (idTimeout > 5) {
+    Serial1.setConsole();
+
+    httpRequest({
+      method: "POST",
+      url: require("Storage").read("telegrame.txt") + "/sendMessage",
+      data: "chat_id=7928495281&text=สวิตซ์ประตูไฟฟ้า: หยุดการแจ้งเตือนชั่วตราว เนื่องจากเกิดข้อผิดพลาดบ่อยมากเกินไป",
+      headers: { "Content-Type": "application/x-www-form-urlencoded" }
+    });
+
+    setTimeout(function () {
+      LoopbackA.setConsole();
+    }, 60000);
+  }
 });
+
+setTimeout(function () {
+  LoopbackA.setConsole();
+}, 10000);
